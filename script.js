aed8ee0228ca7aee02dba3f24485e66565149adb
pprg1996 <pprg1996@gmail.com>
2018-03-30 19:29:36 -0400

    Added dynamic greeting text

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-var wa="web applications";
-var cw="corporate websites";
-var lp="landing pages";
-var ui="user interfaces";
+var currentChar = 0,
+    currentText = 0;
+var endOfWord = false;
+var dynamicText = "";
+var text = ["web applications.", "corporate websites.", "landing pages.", "user interfaces."];
+var dynamicTextTag = $(".dynamicText");
+
+function start() {
+    var sumarLetra = setInterval(() => {
+        dynamicText += text[currentText].charAt(currentChar);
+        currentChar++;
+        dynamicTextTag.html(dynamicText);
+        if (currentChar == text[currentText].length) {
+            currentChar = 0;
+            dynamicText = "";
+            endOfWord = true;
+            clearInterval(sumarLetra);
+            setTimeout(() => {
+                currentText++
+                if (currentText == text.length) currentText = 0;
+                start();
+            }, 2000);
+        }
+    }, 100);
+}
+
+start();
